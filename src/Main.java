d55bc56279c71b0edc8a2f4ee3bbff093ca4b8a6
LeQuangGiaBao <baolqg1309@gmail.com>
2025-05-29 12:11:07 +0700

    Commit01:Add exercise 02 and create menu for methods

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Exercise 1");
-        solvePerimeterRectangle();
-        solveArea();
+        System.out.print("Choose method : ");
+        int method = getInput();
+
+        switch (method) {
+            case 1:
+                System.out.println("Exercise 1");
+                solvePerimeterRectangle();
+                solveArea();
+                break;
+            case 2:
+                System.out.println("Exercise 2");
+                calculateBasicOperations();
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
     }
+
     private static int getInput(){
         while(true){
             try {
@@ -33,4 +48,23 @@ public class Main {
 
         System.out.println("Area of rectangle:"+ (width*height));
     }
+
+    private static void calculateBasicOperations() {
+        System.out.print("Enter first number: ");
+        int number1 = getInput();
+
+        System.out.print("Enter second number: ");
+        int number2 = getInput();
+
+        System.out.println("Sum: " + (number1 + number2));
+        System.out.println("Difference: " + (number1 - number2));
+        System.out.println("Product: " + (number1 * number2));
+
+        if (number2 != 0) {
+            System.out.printf("Quotient: %.2f%n", ((double) number1 / number2));
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+    }
+
 }
